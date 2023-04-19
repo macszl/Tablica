726872f9f5f88f1d6b128ae84cef6c1c8108ac71
Maciek <Maciek@DESKTOP-9E31177>
2023-04-19 09:57:14 +0200

    Add project files.

---
+++ Tablica/FileName.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tablica
+{
+    public class ResizableArray<T>
+    {
+        T[] values;
+        private int count;
+        public void Add(T item)
+        {
+            if (count == values.Length)
+            {
+                Resize(2 * count);
+            }
+
+            values[count++] = item;
+            ItemAdded?.Invoke(this, item);
+            SizeChanged?.Invoke(this, count);
+        }
+
+        private void Resize(int newSize)
+        {
+            T[] newData = new T[newSize];
+
+            for (int i = 0; i < count; i++)
+            {
+                newData[i] = data[i];
+            }
+
+            data = newData;
+            SizeChanged?.Invoke(this, newSize);
+        }
+
+        public int getCount()
+        {
+            return count;
+        }
+    }
+
+    public class Helper
+    {
+        public static bool isEqual(int expected, int actual)
+        {
+
+        }
+    }
+}
---
+++ Tablica/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tablica;
+
+
+
+
+Console.WriteLine("=== ARRAY TESTS ===");
+
+
+//sprawdzamy dodawanie
+Console.WriteLine("Checking if Add works properly.");
+var array = new ResizableArray<int>();
+
+array.Add(1);
+array.Add(2);
+array.Add(3);
+ 
+Console.WriteLine( "Is count equal to 3? " + Helper.isEqual(3, array.getCount()));
+Console.WriteLine( "Is the first element equal to 1? " + Helper.isEqual(1, array[0]));
+Console.WriteLine( "Is the second element equal to 2? " + Helper.isEqual(2, array[1]));
+Console.WriteLine( "Is the third element equal to 3? " + Helper.isEqual(3, array[2]));
+
+
+
+//sprawdzamy indexer
+Console.WriteLine("Checking if the [] operator works properly.");
+array = new ResizableArray<int>();
+
+array.Add(1);
+array.Add(2);
+array.Add(3);
+
+array[1] = 4;
+
+Console.WriteLine("Is the fourth element" + Helper.isEqual(4, array[1]) );
+
+//sprawdzamy czy zostanie zwrocony wyjatek
+array = new ResizableArray<int>();
+bool exceptionWasThrown = false;
+try
+{
+    array[0] = 1;
+}
+catch
+{
+    exceptionWasThrown = true;
+}
+finally
+{
+    String output = exceptionWasThrown ? "Exception was thrown." : "Exception was not thrown.";
+    Console.WriteLine(output);
+}
+
+
+//sprawdzamy resize
+array = new ResizableArray<int>();
+
+array.Add(1);
+array.Add(2);
+array.Add(3);
+
+array[5] = 6;
+
+Helper.isEqual(6, array[5]);
+Helper.isEqual(6, arra
+
+//sprawdzamy eventy
+
+var array = new ResizableArray<int>();
+int count = 0;
+
+array.Changed += (sender, e) => count++;
+
+array.Add(1);
+array[0] = 2;
+
+Helper.isEqual(2, count);
